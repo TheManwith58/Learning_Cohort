46fb178bac4eeb533a5f1207286166299cf300ed
TheManwith58 <harshra979@gmail.com>
2026-06-28 13:45:56 +0530

    added origin routes

--- Week8CourseSellingApp/index.js
+++ Week8CourseSellingApp/index.js
@@ -1,4 +1,24 @@
 const express = require( "express");
 const app = express();
 const jwt = require ("jsonwebtoken");
+app.post("/signup" , (req, res)=>
+{
+
+})
+app.post("/login" , (req, res)=>
+{
+
+})
+app.post("/allCourses" , (req, res)=>
+{
+
+})
+app.post("/myCourses" , (req, res)=>
+{
+
+})
+app.post("/buyCourses" , (req, res)=>
+{
+
+})
 app.listen(3000);
